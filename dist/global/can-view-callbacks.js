de386b24c7f462f96fda471f9e3b462ed92c483b
David Luecke <daff@neyeon.com>
2016-04-25 14:33:45 -0700

    3.0.0-pre.1

---
+++ dist/global/can-view-callbacks.js
@@ -0,0 +1,156 @@
+/*[global-shim-start]*/
+(function(exports, global, doEval){ // jshint ignore:line
+	var origDefine = global.define;
+
+	var get = function(name){
+		var parts = name.split("."),
+			cur = global,
+			i;
+		for(i = 0 ; i < parts.length; i++){
+			if(!cur) {
+				break;
+			}
+			cur = cur[parts[i]];
+		}
+		return cur;
+	};
+	var modules = (global.define && global.define.modules) ||
+		(global._define && global._define.modules) || {};
+	var ourDefine = global.define = function(moduleName, deps, callback){
+		var module;
+		if(typeof deps === "function") {
+			callback = deps;
+			deps = [];
+		}
+		var args = [],
+			i;
+		for(i =0; i < deps.length; i++) {
+			args.push( exports[deps[i]] ? get(exports[deps[i]]) : ( modules[deps[i]] || get(deps[i]) )  );
+		}
+		// CJS has no dependencies but 3 callback arguments
+		if(!deps.length && callback.length) {
+			module = { exports: {} };
+			var require = function(name) {
+				return exports[name] ? get(exports[name]) : modules[name];
+			};
+			args.push(require, module.exports, module);
+		}
+		// Babel uses the exports and module object.
+		else if(!args[0] && deps[0] === "exports") {
+			module = { exports: {} };
+			args[0] = module.exports;
+			if(deps[1] === "module") {
+				args[1] = module;
+			}
+		} else if(!args[0] && deps[0] === "module") {
+			args[0] = { id: moduleName };
+		}
+
+		global.define = origDefine;
+		var result = callback ? callback.apply(null, args) : undefined;
+		global.define = ourDefine;
+
+		// Favor CJS module.exports over the return value
+		modules[moduleName] = module && module.exports ? module.exports : result;
+	};
+	global.define.orig = origDefine;
+	global.define.modules = modules;
+	global.define.amd = true;
+	ourDefine("@loader", [], function(){
+		// shim for @@global-helpers
+		var noop = function(){};
+		return {
+			get: function(){
+				return { prepareGlobal: noop, retrieveGlobal: noop };
+			},
+			global: global,
+			__exec: function(__load){
+				doEval(__load.source, global);
+			}
+		};
+	});
+}
+)({},window,function(__$source__, __$global__) { // jshint ignore:line
+	eval("(function() { " + __$source__ + " \n }).call(__$global__);");
+}
+)
+/*can-view-callbacks@3.0.0-pre.0#can-view-callbacks*/
+define('can-view-callbacks', function (require, exports, module) {
+    var ObserveInfo = require('can-observe-info');
+    var dev = require('can-util/js/dev/dev');
+    var getGlobal = require('can-util/js/global/global');
+    var domMutate = require('can-util/dom/mutate/mutate');
+    var attr = function (attributeName, attrHandler) {
+        if (attrHandler) {
+            if (typeof attributeName === 'string') {
+                attributes[attributeName] = attrHandler;
+            } else {
+                regExpAttributes.push({
+                    match: attributeName,
+                    handler: attrHandler
+                });
+            }
+        } else {
+            var cb = attributes[attributeName];
+            if (!cb) {
+                for (var i = 0, len = regExpAttributes.length; i < len; i++) {
+                    var attrMatcher = regExpAttributes[i];
+                    if (attrMatcher.match.test(attributeName)) {
+                        cb = attrMatcher.handler;
+                        break;
+                    }
+                }
+            }
+            return cb;
+        }
+    };
+    var attributes = {}, regExpAttributes = [], automaticCustomElementCharacters = /[-\:]/;
+    var tag = function (tagName, tagHandler) {
+        if (tagHandler) {
+            if (getGlobal().html5) {
+                getGlobal().html5.elements += ' ' + tagName;
+                getGlobal().html5.shivDocument();
+            }
+            tags[tagName.toLowerCase()] = tagHandler;
+        } else {
+            var cb = tags[tagName.toLowerCase()];
+            if (!cb && automaticCustomElementCharacters.test(tagName)) {
+                cb = function () {
+                };
+            }
+            return cb;
+        }
+    };
+    var tags = {};
+    var callbacks = {
+        _tags: tags,
+        _attributes: attributes,
+        _regExpAttributes: regExpAttributes,
+        tag: tag,
+        attr: attr,
+        tagHandler: function (el, tagName, tagData) {
+            var helperTagCallback = tagData.options.get('tags.' + tagName, { proxyMethods: false }), tagCallback = helperTagCallback || tags[tagName];
+            var scope = tagData.scope, res;
+            if (tagCallback) {
+                res = ObserveInfo.notObserve(tagCallback)(el, tagData);
+            } else {
+                res = scope;
+            }
+            if (res && tagData.subtemplate) {
+                if (scope !== res) {
+                    scope = scope.add(res);
+                }
+                var result = tagData.subtemplate(scope, tagData.options);
+                var frag = typeof result === 'string' ? can.view.frag(result) : result;
+                domMutate.appendChild.call(el, frag);
+            }
+        }
+    };
+    module.exports = callbacks;
+});
+/*[global-shim-end]*/
+(function(){ // jshint ignore:line
+	window._define = window.define;
+	window.define = window.define.orig;
+}
+)();
